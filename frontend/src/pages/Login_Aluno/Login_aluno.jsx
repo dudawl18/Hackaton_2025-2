c8d5c95073f1ccb4e192b25830e7ab4a72dea9c9
dudawl18 <mwolfluiz123a3@gmail.com>
2025-12-03 17:33:43 -0300

    produzindo a tela de login dos responsaveis

--- frontend/src/pages/Login_Aluno/Login_aluno.jsx
+++ frontend/src/pages/Login_Aluno/Login_aluno.jsx
@@ -2,8 +2,33 @@ import "../Login_Aluno/Login_aluno.css"
 
 function Login_aluno() {
   return (
-    <div>
-      <p>Login_Aluno</p>
+    <div className="toda-tela-content">
+
+        <div className="div-superior-logo">
+            <img src="./public/logo.svg" alt="" />
+        </div>
+
+        <div className="div-area-principal-content">
+            <div className="parte-esquerda-content">
+                <div className="legendas-content">
+
+                    <p className="titulo">Bem-Vindo</p>
+                    <p className="descricao">Digite o CPF e a senha do responsável</p>
+
+                </div>
+                <div className="inputs-button-content">
+                    
+                    <input type="text" placeholder="CPF" />
+                    <input type="text" placeholder="Senha"/>
+                    <button>Confirmar</button>
+
+                </div>
+            </div>
+            <div className="parte-direita-content">
+                <img src="" alt="" />
+            </div>
+        </div>
+
     </div>
   )
 }
